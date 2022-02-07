b3bd59a48116d4f773047b3b494e352af5969f41
Clara Villa <Clara.Villa@lego.com>
2022-02-07 13:01:04 +0000

    initial set up

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("nicebreaker JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/nicebreaker";
+
+module.exports = MONGO_URI;
