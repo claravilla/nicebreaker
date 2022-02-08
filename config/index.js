c08d8847d908d4a9866b31c07111eb70fd40b0cd
Clara Villa <Clara.Villa@lego.com>
2022-02-08 10:25:26 +0000

    remove duplicate hbs declaration in config

--- config/index.js
+++ config/index.js
@@ -31,8 +31,6 @@ const hbs = require("hbs");
 // Connects the mongo uri to maintain the same naming structure
 const MONGO_URI = require("../utils/consts");
 
-const hbs = require("hbs");
-
 // Middleware configuration
 module.exports = (app) => {
   // In development environment the app logs
