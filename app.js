90616ca19ea6147233bf3737478c79510d52faef
David Vekony <dave.vekony@gmail.com>
2022-02-08 12:54:11 +0000

    add some styling

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const projectName = "nicebreaker";
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.title = `${capitalized(projectName)}`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
