c8d09b35048e6b4dbaf72da3f50052a1083a63f5
Betilla <52048170+claravilla@users.noreply.github.com>
2022-02-08 14:41:30 +0000

    Cards route (#11)
    * create card route
    
    * edit card route

--- routes/card.js
+++ routes/card.js
@@ -13,7 +13,9 @@ const User = require("../models/User.model");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-router.get("/", (req, res, next) => {
+//GET USER CARDS
+
+router.get("/", isLoggedIn, (req, res, next) => {
   const userId = req.session.user._id; //getting the user ID from the session object
   User.findById(userId)
     .populate("cards")
@@ -27,6 +29,73 @@ router.get("/", (req, res, next) => {
     });
 
   res.render("mycards/my-cards");
+});
+
+//CREATING A NEW CARD
+
+router.get("/new", isLoggedIn, (req, res, next) => {
+  res.render("mycards/create-card");
+});
+
+router.post("/new", (req, res, next) => {
+  const { cardText, labels } = req.body; //getting the data from the form
+  const userId = req.session.user._id; //getting the user ID
+  PrivateCard.create(cardText, labels) //create card in private collection
+    .then((createdCard) => {
+      cardId = createdCard._id;
+      return User.findOneAndUpdate(
+        { id: userId },
+        { $push: { cards: cardId } } //add card to the the user record
+      );
+    })
+    .then(() => res.redirect("/mycards"))
+    .catch((error) => {
+      console.log(error);
+      res.render("mycards/create-card", { errorMessage: error });
+    });
+});
+
+//EDITING AN EXISTING CARD
+router.get("/:id/edit", (req, res, next) => {
+  const { cardId } = req.params;
+  PrivateCard.findById({ _id: cardId }).then((card) => {
+    res.render("mycards/edit-card", { card });
+  });
+});
+
+router.post("/:id/edit", isLoggedIn, (req, res, next) => {
+  const { cardId } = req.params;
+  const { cardText, labels, hasWorked, hasNotWorked } = req.body;
+  PrivateCard.findByIdAndUpdate(
+    { _id: cardId },
+    { cardText, labels, hasWorked, hasNotWorked },
+    { new: true }
+  )
+    .then((card) => {
+      console.log("card had been update: ", card);
+      res.redirect("/");
+    })
+    .catch((error) => {
+      res.render("mycards/edit-card", { errorMessage: error });
+    });
+});
+
+//DELETING AN EXISTING CARD
+
+//rendering the modal when the delete button is created
+router.get("/:id/delete", isLoggedIn, (req, res, next) => {
+  const cardId = req.params;
+  res.render("mycards/modal");
+});
+
+//delete the card
+
+router.post("/:id/delete", isLoggedIn, (req, res, next) => {
+  const cardId = req.params;
+  PrivateCard.deleteOne({ _id: cardId }).then(() => {
+    console.log("card has been deleted");
+    res.redirect("mycards");
+  });
 });
 
 module.exports = router;
