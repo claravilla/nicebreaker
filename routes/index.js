2cd83ea1e9a0c38837a1eb2a4973b8fb760defee
Clara Villa <Clara.Villa@lego.com>
2022-02-09 08:03:17 +0000

    add index route with random card

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,18 @@ const PublicCard = require("../models/public.card.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  // PublicCard.findOneRandom(function (err, randomCard) {
-  //   //select a random card from the public collection
-  //   console.log(req.session.user);
-  //   if (!err) {
-  //     console.log(randomCard);
-  //     res.render("index", {data : {randomCard: randomCard, user : req.session.user}});
-  //   } else {
-  //     console.log(err);
-  //   }
-  // });
+  PublicCard.findOneRandom(function (err, randomCard) {
+    //select a random card from the public collection
+    console.log(req.session.user);
+    if (!err) {
+      console.log(randomCard);
+      res.render("index", {
+        data: { randomCard: randomCard, user: req.session.user },
+      });
+    } else {
+      console.log(err);
+    }
+  });
   res.render("index");
 });
 
