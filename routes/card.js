336fae1c6e3c2706f1fede34ee7660db21e6f35e
David Vekony <dave.vekony@gmail.com>
2022-02-09 15:24:27 +0000

    connect cards to BE

--- routes/card.js
+++ routes/card.js
@@ -40,8 +40,22 @@ router.post("/new", (req, res, next) => {
   let keys = Object.keys(req.body); //getting the label from the body
   let listOfKeys = keys.map((elem) => elem);
   let label = listOfKeys.splice(1);
-  const [SFW, dinnerTable, nightOut, firstDate] = label;
-
+  let SFW = false,
+    dinnerTable = false,
+    nightOut = false,
+    firstDate = false;
+  if (label.includes("SFW")) {
+    SFW = true;
+  }
+  if (label.includes("dinner table")) {
+    dinnerTable = true;
+  }
+  if (label.includes("night out")) {
+    nightOut = true;
+  }
+  if (label.includes("first date")) {
+    firstDate = true;
+  }
   const cardText = req.body.cardText; //getting the cardText from the body
   const userId = req.session.user._id; //getting the user ID
   PrivateCard.create({ cardText, SFW, dinnerTable, nightOut, firstDate }) //create card in private collection
@@ -100,9 +114,9 @@ router.post("/:id/edit", isLoggedIn, (req, res, next) => {
 
 router.post("/:id/delete", isLoggedIn, (req, res, next) => {
   const cardId = req.params;
-  PrivateCard.deleteOne({ _id: cardId }).then(() => {
+  PrivateCard.deleteOne({ cardId }).then(() => {
     console.log("card has been deleted");
-    res.redirect("mycards");
+    res.redirect("/mycards");
   });
 });
 
